71de888661e2ad870afbf07a773b134a3f64983d
Tiger255 <45604086+Tiger255@users.noreply.github.com>
2018-12-04 18:30:46 +0100

    Add files via upload

--- data1.py
+++ data1.py
@@ -1,5 +1,6 @@
 from chicken_dinner.pubgapi import PUBG
 from chicken_dinner.pubgapi import PUBGCore
+import json
 
 def Apikey():
     api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5OGNiYzI3MC1iYTlkLTAxMzYtNjg3MS02YjU5NWYzNGI3NjciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTQwNDgzNDc3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImJwYXAifQ.hr--q6zWIXB6Fkba3XGhgMaQUubCo9vn1h1YgMm1dVk"
@@ -22,3 +23,26 @@ def Player_Stats(playername):
     pubgcore = PUBGCore(api_key, "pc-na")
     shroud = pubgcore.players("player_names", "shroud")
     print(shroud)
+
+
+
+
+
+api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5OGNiYzI3MC1iYTlkLTAxMzYtNjg3MS02YjU5NWYzNGI3NjciLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTQwNDgzNDc3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6ImJwYXAifQ.hr--q6zWIXB6Fkba3XGhgMaQUubCo9vn1h1YgMm1dVk"
+pubgcore = PUBGCore(api_key, "pc-eu")
+shroud = pubgcore.players("player_names", "Vengaard")
+
+#print(shroud["data"])
+#print(type(shroud['data']))
+
+for data in shroud['data']:
+   id = (data['id'])
+   #print(id)
+
+lifetime = pubgcore.lifetime(id)
+print(lifetime)
+new = json.dumps(lifetime, indent=2)
+print(new)
+
+for data in lifetime['data']:
+   print(data['duo'])
